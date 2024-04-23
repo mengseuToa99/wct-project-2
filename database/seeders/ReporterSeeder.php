ce79e2b69041a4e11ff3382e4481619fce995327
Lymengschool <tek.lymeng.2821@rupp.edu.kh>
2024-04-23 21:39:26 +0700

    seed

--- database/seeders/ReporterSeeder.php
+++ database/seeders/ReporterSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\reporter;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class ReporterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        reporter::factory()
+        ->count(5)
+        ->hasReports(5)
+        ->create();
+        
     }
 }
