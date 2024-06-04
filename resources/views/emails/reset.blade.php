ce8f24b474e9ed243d26ce5b5525bdd2c6c2fbfe
mengseuTao99 <taimengseu7777gmail.com>
2024-06-04 09:41:30 +0700

    go the login page to reset password

--- resources/views/emails/reset.blade.php
+++ resources/views/emails/reset.blade.php
@@ -11,7 +11,7 @@
     <p>Hello {{ $user->email }},</p>
     <p>Your new temporary password is: {{ $password }}</p>
     <p>Please use the following link to reset your password:</p>
-    <a href="http://localhost:3000/login">Reset Password</a>
+    <a href="https://jomyeak.vercel.app/login">Reset Password</a>
     <p>If you did not request a password reset, please ignore this email.</p>
 </body>
 </html>
